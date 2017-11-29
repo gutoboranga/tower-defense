0083c2e77cf05bfc7532b6f058ae259f223f4d1d
Rodrigo Scroferneker <rodrigo.scroferneker@gmail.com>
2017-11-29 19:10:11 -0200

    Torres girando e sendo removidas

---
+++ object-oriented/TowerDefense/TowerDefense/Models/TowerMenu.swift
@@ -0,0 +1,13 @@
+//
+//  TowerMenu.swift
+//  TowerDefense
+//
+//  Created by Rodrigo Franzoi Scroferneker on 29/11/17.
+//  Copyright © 2017 gatosDeSchnorrdinger. All rights reserved.
+//
+
+import Cocoa
+
+class TowerMenu: SKSpriteNode {
+
+}
